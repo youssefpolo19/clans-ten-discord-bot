baa341d389ee03353bfb71e03bdd4da29fab85a0
youssefpolo19 <154542782+youssefpolo19@users.noreply.github.com>
2024-12-19 10:20:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-module.exports = {
-"token": "MTI3NzI0MTc4MDE1NjM3MTA4NQ.GqlqEW.t9aJy74VnU1gW5k8VYssFjVEvwfuZiaXGX7f3g", // token bot
-"prefix": "-", // prefix bot
-"categoryId": "1277242169039786018" // category ticketId
-}
+
